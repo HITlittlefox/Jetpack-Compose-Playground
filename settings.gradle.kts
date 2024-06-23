6287413dd4f111f7fcebcc9f9c165e7527245b1a
HITlittlefox <963614756@qq.com>
2024-06-23 14:38:54 +0800

    更新gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,14 @@
 dependencyResolutionManagement {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri(" https://maven.aliyun.com/repository/google") }
+        maven { url = uri(" https://maven.aliyun.com/repository/gradle-plugin") }
+        mavenLocal()
         mavenCentral()
         google()
+        // jcenter()
+        maven { url = uri("https://jitpack.io") }
+        mavenCentral() { url = uri("https://jcenter.bintray.com/") }
 
     }
 }
